fb6914a439ea6df292d40c967fdff74ec3894a33
cipherxsniper <simpl3hoods@gmail.com>
2026-04-16 12:31:29 -0700

    Ω auto-sync: 2026-04-16 12:31:29

--- omega_kernel_v6_8.py
+++ omega_kernel_v6_8.py
@@ -4,3 +4,7 @@ from omega_kernel_v6_8_stability import OmegaStabilityLayer
 
 self.stability = OmegaStabilityLayer()
 
+
+log_data["locked_node"] = self.stability.locked_node
+log_data["learning_rate"] = self.learning_rate
+
