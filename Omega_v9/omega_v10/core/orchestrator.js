dee5ba0658dc793256e2d06ad3b3379170bcc8ae
cipherxsniper <simpl3hoods@gmail.com>
2026-04-16 13:27:45 -0700

    Ω auto-sync: 2026-04-16 13:27:44

--- Omega_v9/omega_v10/core/orchestrator.js
+++ Omega_v9/omega_v10/core/orchestrator.js
@@ -1,18 +1,19 @@
 const Redis = require("redis");
-const EventStore = require("./event_store");
-
-const store = new EventStore();
 
 (async () => {
   const sub = Redis.createClient();
   await sub.connect();
 
+  let count = 0;
+  let avg = 0;
+
   await sub.subscribe("omega_stream", (msg) => {
     const data = JSON.parse(msg);
 
-    store.append(data);
+    avg = (avg * count + (data.confidence || 0)) / (count + 1);
+    count++;
 
-    console.log("📡 SWARM:", data.node, data.confidence);
+    console.log("📊 Swarm:", avg.toFixed(3));
   });
 
   console.log("🧠 Omega v10 Core ONLINE");
